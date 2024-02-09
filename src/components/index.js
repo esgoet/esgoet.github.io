a005d429f482badc9a559d3c979b5f78777332f5
esgoet <toddesgo@gmail.com>
2024-02-09 13:35:59 +0100

    reupload

---
+++ src/components/index.js
@@ -0,0 +1,26 @@
+import { SatelliteCanvas, StarsCanvas, MoonCanvas } from './canvas';
+import Hero from './Hero';
+import Navbar from './Navbar';
+import About from './About';
+import Experience from './Experience';
+import Works from './Works';
+import Feedbacks from './Feedbacks';
+import Contact from './Contact';
+import Footer from './Footer';
+import SocialLinks from './SocialLinks';
+
+
+export {
+  Hero,
+  Navbar,
+  About,
+  Experience,
+  Works,
+  Feedbacks,
+  Contact,
+  SatelliteCanvas, 
+  StarsCanvas,
+  MoonCanvas,
+  Footer,
+  SocialLinks
+}
---
+++ src/styles.js
@@ -0,0 +1,17 @@
+const styles = {
+  paddingX: "sm:px-16 px-6",
+  paddingY: "sm:py-16 py-6",
+  padding: "sm:px-16 px-6 sm:py-16 py-10",
+
+  heroHeadText:
+    "font-black text-secondary xl:text-[80px] lg:text-[70px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2",
+  heroSubText:
+    "text-secondary font-medium xl:text-[24px] lg:text-[22px] sm:text-[20px] xs:text-[16px] text-[16px] lg:leading-[36px]",
+
+  sectionHeadText:
+    "text-primary font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[40px] mb-2 drop-shadow-text",
+  sectionSubText:
+    "sm:text-[18px] text-[14px] text-tertiary uppercase tracking-wider -mb-2",
+};
+
+export { styles };
